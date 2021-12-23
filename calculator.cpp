93d0163679545d9bf05067267fbe95352b3b6ab0
Bereket <58364506+zgreatking@users.noreply.github.com>
2021-12-23 19:01:14 +0300

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,11 @@
+/*
+[SolBy: Bereket Siraw]
+[Date: 12/23/2021]
+[4 more Info:TG->@thunder_is_taken]
+[Privacy and Policy:You are not allowed to modify or re-use the code without the author's permission]
+*/
+#include <iostream> 
+int main(char __XO,double* __VL=new double[0x2]){ //[You can only use - * + / operators]
+    std::cin>>__VL[0x0]>>*&__XO>>__VL[0x1];
+    std::cout<<((__XO==0x2b)?(__VL[0x0])+(__VL[0x1]):(__XO==0x2d)?(__VL[0x0])-(__VL[0x1]):(__XO==0x2f)?(__VL[0x0])/(__VL[0x1]):(__XO==0x2a)?(__VL[0x0])*(__VL[0x1]):0x194);
+}
