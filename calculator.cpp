3d873a97f700339fb46e12081ce097c4a97fab89
Bereket Siraw <58364506+zgreatking@users.noreply.github.com>
2022-01-02 16:34:28 +0300

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -4,7 +4,7 @@
 [4 more Info:TG->@thunder_is_taken]
 */
 #include <iostream> 
-int main(char __XO,double* __VL=new double[0x2]){ //[You can only use - * + / operators]
-    std::cin>>__VL[0x0]>>*&__XO>>__VL[0x1];
-    std::cout<<((__XO==0x2b)?(__VL[0x0])+(__VL[0x1]):(__XO==0x2d)?(__VL[0x0])-(__VL[0x1]):(__XO==0x2f)?(__VL[0x0])/(__VL[0x1]):(__XO==0x2a)?(__VL[0x0])*(__VL[0x1]):0x194);
+int main(char __XO,int x, int y){ //[You can only use - * + / operators]
+    std::cin>>x>>__XO>>y;
+    std::cout<<((__XO==0x2b)?x+y:(__XO==0x2d)?x-y:(__XO==0x2f)?x/y:(__XO==0x2a)?x*y:0x194);
 }
